7be6a0e3111160384346048b1e33bcc02c536a82
freedom5566 <s1410231034@gms.nutc.edu.tw>
2018-04-05 18:10:39 +0800

    :art: 新增文章驗證

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # thinkphp5.1
 
 實作CURD
+
+***
+5.1環境要求
+> PHP >= 5.6.0      
+  PDO PHP Extension     
+  MBstring PHP Extension
+
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,6 +1,6 @@
 <?php
 namespace app\index\controller;
-//use think\Controller;
+
 class Index
 {
     public function index()
@@ -8,8 +8,4 @@ class Index
         return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family:"Hi Melody", cursive; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 100px }</style><div style="padding: 24px 48px;"> <h1> (๑•̀ω•́)ノ</h1><p> ThinkPHP V5.1<br/><span style="font-size:50px">12載初心不改（2006-2018） - 你值得信賴的PHP框架</span></p></div><link href="https://fonts.googleapis.com/css?family=Hi+Melody" rel="stylesheet">';
     }
 
-    // public function hello($name = 'ThinkPHP5')
-    // {
-    //     return 'hello,' . $name;
-    // }
 }
